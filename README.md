27db89c9a0a71b04c666bc383f597e894ce9703d
Ben Mittelberger <bmittelb@stanford.edu>
2016-01-18 16:41:52 -0800

    added link to other repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # trickle-server
 Server code for trickle
+
+Link to front-end repository: https://github.com/bmittelberger/trickle
--- local.js
+++ local.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello!');
+  res.send('<html><h1>Hello!</h1></html>');
 });
 
 app.listen(3000);
