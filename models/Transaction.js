3e9458e846ea1ae82d0513fc6cfdc404bdeaca57
Ben Mittelberger <bmittelb@stanford.edu>
2016-02-09 01:04:49 -0800

    added some demo code stuff

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,4 +1,8 @@
 var transactionUtils = require('../utils/transaction.js');
+// var venmoUtils = require('../utils/venmo.js')
+var Transaction = models.Transaction;
+
+
 
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('Transaction', {
@@ -23,9 +27,35 @@ module.exports = function(sequelize, DataTypes) {
     }
   }, {
     hooks: {
-      // afterCreate: function(transaction, options, cb) {
-      //   transactionUtils.processTransaction(transaction, cb);
-      // }
+      afterCreate: function(transaction, options, cb) {
+        // transactionUtils.processTransaction(transaction, cb);
+        if (transaction.amount > 100) {
+          transaction
+            .updateAttributes({
+              status : 'DECLINED',
+              message : 'The posted amount was over the demo amount of $100. Please try not being so greedy.'
+            })
+            .then(function(transaction) {
+              cb();
+            })
+            .catch(function(err) {
+              cb();
+            });
+        } else {
+          transaction
+            .updateAttributes({
+              status : 'APPROVED',
+              message : 'Your transaction passed the auto-reimburse rules.'
+            })
+            .then(function(transaction) {
+              venmoUtils.reimburse(transaction);
+              cb();
+            })
+            .catch(function(err) {
+              cb();
+            });
+        }
+      }
     }
   });
 };
