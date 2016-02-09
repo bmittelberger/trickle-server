23abc89338130e70fa3ee9848529f0557bc82cbe
Kevin Moody <Kevin@DN51usra.SUNet>
2016-02-09 01:03:47 -0800

    Venmo API integration

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,11 @@
 var auth = require('./auth.js'),
-    transaction = require('./transaction.js');
+    transaction = require('./transaction.js'),
+    venmo = require('./venmo.js');
 
 module.exports = function(models, config) {
   return {
     auth: auth(models, config),
-    transaction: transaction(models, config)
+    transaction: transaction(models, config),
+    venmo: venmo(models, config)
   };
 };
